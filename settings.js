35ca8233a0292fbb493b954b1994f11523abd0a6
vidu-27 <107163661+vidu-27@users.noreply.github.com>
2022-06-09 09:02:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
+global.owner = ['94710515739']
+global.premium = ['94710515739']
+global.ownernomer = '94710515739'
+global.ownername = 'vidu-27'
 global.botname = 'Bixby MD'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/nexusNw'
